19d6a2b8c2c4c795e21bd33faad2c82092674476
shomiyu <m1011522@gmail.com>
2022-03-29 00:27:02 +0900

    テスト

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,11 +17,12 @@ export default defineNuxtConfig({
 
   privateRuntimeConfig: {
     // baseURL: BASE_URL,
-    apiKey: API_KEY,
+    // apiKey: API_KEY,
   },
 
   publicRuntimeConfig: {
     // baseURL: BASE_URL,
-    apiKey: process.env.NODE_ENV !== "production" ? API_KEY : undefined,
+    apiKey: API_KEY,
+    // apiKey: process.env.NODE_ENV !== "production" ? API_KEY : undefined,
   },
 });
