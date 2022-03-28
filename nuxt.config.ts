f5ac56ca481dd635477adeb3911e24a26e00c923
shomiyu <m1011522@gmail.com>
2022-03-28 20:50:29 +0900

    さらに検証

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,11 +6,12 @@ export default defineNuxtConfig({
    * See: https://v3.nuxtjs.org/docs/directory-structure/nuxt.config#css
    */
   css: [],
-  privateRuntimeConfig: {
-    apiKey: API_KEY,
-  },
   publicRuntimeConfig: {
-    baseURL: BASE_URL,
+    baseURL: process.env.NODE_ENV !== "production" ? BASE_URL : undefined,
     apiKey: process.env.NODE_ENV !== "production" ? API_KEY : undefined,
+  },
+  privateRuntimeConfig: {
+    baseURL: BASE_URL,
+    apiKey: API_KEY,
   },
 });
